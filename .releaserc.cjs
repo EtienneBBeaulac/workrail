27a8c8dbc0701fc399ff00225d715a04cd120cf5
Etienne Beaulac <etiennebeaulac@gmail.com>
2026-04-07 17:55:59 -0600

    chore: add semantic-release/git to commit version bump back to repo, sync version to 3.16.0

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -54,6 +54,13 @@ module.exports = {
         publishCmd: "npm publish --access public"
       }
     ],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["package.json"],
+        message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+      }
+    ],
     "@semantic-release/github"
   ]
 };
